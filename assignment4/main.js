9775d933426c6df95116c73859a1c9fc224012af
nezer293 <akinrinadeebenezer2@gmail.com>
2025-07-31 19:53:47 -0400

    update main.js

--- assignment4/main.js
+++ assignment4/main.js
@@ -1,7 +1,5 @@
 console.log('pt1');
 
-console.log('pt1');
-
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
