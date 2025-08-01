223d2694ee517dc817032a9956759b9fa2970f91
nezer293 <akinrinadeebenezer2@gmail.com>
2025-07-31 23:11:11 -0400

    update on indet.js

--- assignment4/indet.js
+++ assignment4/indet.js
@@ -9,7 +9,7 @@ const height = (canvas.height = window.innerHeight);
 // function to generate random number
 
 function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 // function to generate random color
