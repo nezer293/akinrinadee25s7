2a5b0d2843fb521ff9d11a6739655f4b8aba856f
nezer293 <akinrinadeebenezer2@gmail.com>
2025-07-31 22:48:29 -0400

    update on mainn.js

--- assignment4/mainn.js
+++ assignment4/mainn.js
@@ -36,7 +36,7 @@ btn.addEventListener('click', () => {
   if (btn.getAttribute('class') === 'dark') {
     btn.setAttribute('class', 'light');
     btn.textContent = 'Lighten';
-    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)'
   } else {
     btn.setAttribute('class', 'dark');
     btn.textContent = 'Darken';
